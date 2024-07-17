5c25767340857277a7276a53f16690ac5d141816
Deepika P <120781809+deepuzz11@users.noreply.github.com>
2024-07-17 19:54:49 +0530

    Update beam.py

--- beam.py
+++ beam.py
@@ -2,43 +2,43 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 def create_Graph():
-  n=int(input("Enter no of nodes: "))
-  nodes=[]
-  Heuristic={}
+    n = int(input("Enter number of nodes: "))
+    nodes = []
+    Heuristic = {}
 
-  G = nx.DiGraph()
+    G = nx.DiGraph()
 
-  for i in range(n):
-    node=input(f'Enter node {i+1}: ')
-    nodes.append(node)
+    for i in range(n):
+        node = input(f'Enter node {i+1}: ')
+        nodes.append(node)
 
-  start=input('Enter Starting node: ')
-  goal=input('Enter Goal node: ')
+    start = input('Enter Starting node: ')
+    goal = input('Enter Goal node: ')
 
-  for i in range(n):
-    neighbours=int(input(f"Enter number of neighbours of {nodes[i]}: "))
-    for j in range(neighbours):
-      neighbour=input(f"Enter neighbour {j+1}: ")
-      G.add_edge(nodes[i],neighbour)
+    for i in range(n):
+        neighbours = int(input(f"Enter number of neighbours of {nodes[i]}: "))
+        for j in range(neighbours):
+            neighbour = input(f"Enter neighbour {j+1}: ")
+            G.add_edge(nodes[i], neighbour)
 
-  for node in nodes:
-    if node==goal:
-      Heuristic.update({goal:0})
-    else:
-      h=int(input(f"Enter Heuristic from {node} to {goal}: "))
-      Heuristic.update({node:h})
+    for node in nodes:
+        if node == goal:
+            Heuristic.update({goal: 0})
+        else:
+            h = int(input(f"Enter Heuristic from {node} to {goal}: "))
+            Heuristic.update({node: h})
 
-  return G,start,goal,Heuristic
+    return G, start, goal, Heuristic
 
-G,start,goal,Heuristic=create_Graph()
+G, start, goal, Heuristic = create_Graph()
 
-nx.draw(G,with_labels=True,node_size=1000,node_color='purple',font_size=10)
+nx.draw(G, with_labels=True, node_size=1000, node_color='purple', font_size=10, pos=nx.circular_layout(G))
 plt.show()
 
-def beam(graph, start, goal, beam_width,heuristic):
+def beam_search(graph, start, goal, beam_width, heuristic):
     initial_path = [start]
     beam = [(initial_path, heuristic[start])]
-    beam_paths=[]
+    beam_paths = []
 
     while beam:
         candidates = []
@@ -55,21 +55,24 @@ def beam(graph, start, goal, beam_width,heuristic):
 
         for path, cost in beam:
             if path[-1] == goal:
-              return path
+                return path
 
     return None
 
-width=int(input("Enter beam_width: "))
+width = int(input("Enter beam width: "))
 
-beam_path=beam(G,start,goal,width,Heuristic)
+beam_path = beam_search(G, start, goal, width, Heuristic)
 
-edges_in_beam_path=[(beam_path[i],beam_path[i+1]) for i in range(len(beam_path)-1)]
+if beam_path:
+    edges_in_beam_path = [(beam_path[i], beam_path[i+1]) for i in range(len(beam_path) - 1)]
 
-pos=nx.spring_layout(G)
+    pos = nx.circular_layout(G)
 
-node_labels = {node: node for node in G.nodes()}
-nx.draw(G,pos,node_size=1000,node_color=(1,0.2,0.7),font_size=10)
-nx.draw_networkx_labels(G, pos, labels=node_labels, font_size=10)
-nx.draw_networkx_edges(G, pos, edgelist=edges_in_beam_path, edge_color='orange', width=2)
-plt.title("Beam Visualisation")
-plt.show()
+    node_labels = {node: node for node in G.nodes()}
+    nx.draw(G, pos, node_size=1000, node_color='purple', font_size=10)
+    nx.draw_networkx_labels(G, pos, labels=node_labels, font_size=10)
+    nx.draw_networkx_edges(G, pos, edgelist=edges_in_beam_path, edge_color='orange', width=2)
+    plt.title("Beam Search Visualization")
+    plt.show()
+else:
+    print("No path found")
